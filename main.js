7482f87cceccfc7762339fef542f967efcb9f904
andrewdroz <andrewdroz@gmail.com>
2018-09-28 22:10:52 +0800

    Remove document.ready()
    By including javascript at end of body, DOM would be loaded before any javascript runs. Therefore, a DOM ready check is not needed.

--- main.js
+++ main.js
@@ -1,34 +1,31 @@
+// WRAP <td> INSIDE <div>
+$('.table-cell-resizable td').wrapInner('<div></div>');
 
-$(document).ready(() => {
-  // WRAP <td> INSIDE <div>
-  $('.table-cell-resizable td').wrapInner('<div></div>');
-
-  // ON-LOAD: APPLY BUTTON ONLY TO CELLS ABOVE SPECIFIED HEIGHT
-  $('.table-cell-resizable div').each(function addBtnTgglClass() {
-    if ($(this).find('br').length > 3) {
-      $(this).prepend('<button class="cellResize-btn">+</button>');
-      $(this).toggleClass('reduceCellHeight');
-    }
-  });
-
-  // BUTTON CLICK HANDLER
-  $('.cellResize-btn')
-    .unbind()
-    .click(function handlePlusMinusBtn() {
-      const $rowInQuestion = $(this).closest('tr');
-
-      $rowInQuestion
-        .find('div')
-        .toggleClass('reduceCellHeight');
-
-      $rowInQuestion
-        .find('button')
-        .each(function switchBtnSign() {
-          if ($(this).html() === '+') {
-            $(this).html('-');
-          } else {
-            $(this).html('+');
-          }
-        });
-    });
+// ON-LOAD: APPLY BUTTON ONLY TO CELLS ABOVE SPECIFIED HEIGHT
+$('.table-cell-resizable div').each(function addBtnTgglClass() {
+  if ($(this).find('br').length > 3) {
+    $(this).prepend('<button class="cellResize-btn">+</button>');
+    $(this).toggleClass('reduceCellHeight');
+  }
 });
+
+// BUTTON CLICK HANDLER
+$('.cellResize-btn')
+  .unbind()
+  .click(function handlePlusMinusBtn() {
+    const $rowInQuestion = $(this).closest('tr');
+
+    $rowInQuestion
+      .find('div')
+      .toggleClass('reduceCellHeight');
+
+    $rowInQuestion
+      .find('button')
+      .each(function switchBtnSign() {
+        if ($(this).html() === '+') {
+          $(this).html('-');
+        } else {
+          $(this).html('+');
+        }
+      });
+  });
